6c59a9bb572392fa4afa76e042585971cf7d718d
akalankapagoda <akalankapagoda@gmail.com>
2022-06-01 23:47:53 +0530

    Update material ui components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
 import Button from '@mui/material/Button';
+import Dashboard from './dashboard/Dashboard';
 
 function App() {
-  return <Button variant="contained">Hello World</Button>;
+  return <Dashboard />;
 }
 
 
---
+++ src/dashboard/Dashboard.js
@@ -0,0 +1,123 @@
+import * as React from 'react';
+import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Link from '@mui/material/Link';
+import Orders from './Orders';
+import FileUpload from './FileUpload';
+
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const drawerWidth = 240;
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(['width', 'margin'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const mdTheme = createTheme();
+
+function DashboardContent() {
+  const [open, setOpen] = React.useState(true);
+  const toggleDrawer = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <ThemeProvider theme={mdTheme}>
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        <AppBar>
+          <Toolbar>
+            <Typography
+              component="h1"
+              variant="h4"
+              color="inherit"
+              align="center"
+              noWrap
+              sx={{ flexGrow: 1 }}
+            >
+              Transaction Reconciliation
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Box
+          component="main"
+        >
+          <Toolbar />
+          <Container maxWidth="100%" sx={{ mt: 4, mb: 4 }}>
+            <Grid container spacing={3}>
+              {/* Source File */}
+              <Grid item xs={12} md={4} lg={5}>
+                <Paper
+                  sx={{
+                    p: 2,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    height: 240
+                  }}
+                >
+                  <FileUpload title="Select source file" />
+                </Paper>
+              </Grid>
+              {/* Target File */}
+              <Grid item xs={12} md={4} lg={5}>
+                <Paper
+                  sx={{
+                    p: 2,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    height: 240,
+                  }}
+                >
+                  <FileUpload title="Select target file" />
+                </Paper>
+              </Grid>
+              {/* Recent Orders */}
+              <Grid item xs={12}>
+                <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+                  <Orders />
+                </Paper>
+              </Grid>
+            </Grid>
+            <Copyright sx={{ pt: 4 }} />
+          </Container>
+        </Box>
+      </Box>
+    </ThemeProvider>
+  );
+}
+
+export default function Dashboard() {
+  return <DashboardContent />;
+}
